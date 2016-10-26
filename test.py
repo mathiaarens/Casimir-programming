d581047285c94d3b0b6b23f93c51f357defada04
LauraRpo <laurarpo@gmail.com>
2016-10-26 11:57:11 +0000

    Commit three

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 print("hello world")
 
+
 import numpy as np
 
 
 def circumference_circle(x,y):
+    """This is a function that calculates the circumference of a circle if you enter the radius"""
     y = 2 * x * np.pi
     return y
 
