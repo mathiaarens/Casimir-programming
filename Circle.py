48fad5f5ad0f048ca8a43a03778657e7ab1f4acd
LauraRpo <laurarpo@gmail.com>
2016-10-26 13:02:45 +0000

    commit circle

---
+++ Circle.py
@@ -0,0 +1,17 @@
+
+import matplotlib as mat
+#matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+plt.axes()
+
+def circleplot(radius):
+    circle = plt.Circle((0,0),radius, fc='y')    
+    plt.gca().add_patch(circle)
+    #plt.savefig('circle.png')
+    plt.show()
+    
+#plt.axis('scaled')
+    
+circleplot(1)
+
